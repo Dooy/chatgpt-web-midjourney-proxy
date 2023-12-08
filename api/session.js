275ecee8d060d2a22d11f270977f51e5d200e5d6
Dooy <dooy520@qq.com>
2023-12-08 19:37:49 +0800

    2.12.3

--- api/session.js
+++ api/session.js
@@ -1,13 +1,10 @@
 module.exports = (req, res) => {
   console.log('session.js', req.body);
   try {
-    let data = req.body.data;
-    data.env = _LibsCommon.parseEnv(data.env);
-    new _LibsFlowCommon.core().main(data, (tmpInputReport, tmpInputHistory) => {
-      res.writeHead(200).end(
+    let data = req.body.data; 
+    res.writeHead(200).end(
         JSON.stringify({"a":"b"})
-      );
-    });
+    );
   } catch (e) {
     console.error('session.js', e, req.body);
   }
