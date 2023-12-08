81d6afb39b2186c34be142b7802b8ec9ef40b06a
Dooy <dooy520@qq.com>
2023-12-08 19:49:10 +0800

    2.12.3

--- api/session.js
+++ api/session.js
@@ -2,8 +2,18 @@ module.exports = (req, res) => {
   console.log('session.js', req.body);
   try {
     let data = req.body.data; 
+    let obj ={
+        "status": "Success",
+        "message": "",
+        "data": {
+            "isHideServer": false,
+            "isUpload": false,
+            "auth": false,
+            "model": "ChatGPTAPI"
+        }
+    }
     res.writeHead(200).end(
-        JSON.stringify({"a":"b"})
+        JSON.stringify( obj )
     );
   } catch (e) {
     console.error('session.js', e, req.body);
