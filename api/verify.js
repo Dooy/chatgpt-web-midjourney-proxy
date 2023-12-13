98f737284a9837128b82644ad391b9a063128c1b
Dooy <dooy520@qq.com>
2023-12-13 22:00:46 +0800

    2.12.7

--- api/verify.js
+++ api/verify.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
-    let obj= {"status":"Fail","message":"密钥无效 | Secret key is invalid","data":null}
+    let obj= {"status":"Fail","message":"密钥无效 | Secret key is invalid","data":null};
+    if( req.body &&  req.body.token && process.env.AUTH_SECRET_KEY == req.body.token)  obj= { status: 'Success', message: 'Verify successfully', data: null }
     res.writeHead(200).end(
         JSON.stringify( obj )
     );
