376aae0ee81095ee43f83451e370bdf6c1f8821c
Dooy <dooy520@qq.com>
2024-02-20 00:14:04 +0800

    v2.15.5

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -31,7 +31,7 @@ export default {
   chat: {
     newChatButton: '新建聊天',
     //placeholder: '来说点什么吧...（Shift + Enter = 换行，"/" 触发提示词）',
-    placeholder: '可输入说点什么，也可贴截图或拖拽文件（Shift + Enter = 换行，"/" 触发提示词，"@" 触发最近用过的GPTs）',
+    placeholder: '可输入说点什么，也可贴截图或拖拽文件（Shift + Enter = 换行，"/" 触发提示词）',
     placeholderMobile: '来说点什么...',
     copy: '复制',
     copied: '复制成功',
