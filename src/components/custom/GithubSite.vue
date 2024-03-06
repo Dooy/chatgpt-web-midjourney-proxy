3980acd134c604c6ba8d6eb6c545cac0d9542f74
yzq313 <67686611+yzq313@users.noreply.github.com>
2024-03-06 23:00:47 +0800

    Update GithubSite.vue
    联系

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>与我联系</span>
+    <a href="https://flowus.cn/ff12fa9c-4cd6-4d8a-b3ab-97c321e77617" target="_blank" class="text-blue-500">
+      联系方式
     </a>
   </div>
 </template>
