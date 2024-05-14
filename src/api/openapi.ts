a97e2e00987b643eae348903cc22e9cd7138b92d
Dooy <dooy520@qq.com>
2024-05-14 14:51:02 +0800

    gpt-4o

--- src/api/openapi.ts
+++ src/api/openapi.ts
@@ -20,10 +20,9 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gpt-4-0125-preview": "2023-12",
   "gpt-4-vision-preview": "2023-04",
   "gpt-4-turbo-2024-04-09": "2023-12", 
-  "gpt-4o-2024-05-13": "2023-12", 
-  "gpt-4o": "2023-12", 
-  "gpt-4-turbo": "2023-12",
-  //gpt-4-turbo-2024-04-09
+  "gpt-4o-2024-05-13": "2023-10", 
+  "gpt-4o": "2023-10", 
+  "gpt-4-turbo": "2023-12", 
   "gpt-4-turbo-preview": "2023-12",
   "claude-3-opus-20240229": "2023-08",
   "claude-3-sonnet-20240229": "2023-08",
