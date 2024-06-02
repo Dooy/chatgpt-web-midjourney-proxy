658eab0183c8db3f6723bba5abec3885471acbbb
Dooy <dooy520@qq.com>
2024-06-02 13:27:34 +0800

    v2.18.2.11

--- src/store/modules/chat/index.ts
+++ src/store/modules/chat/index.ts
@@ -201,7 +201,7 @@ export const useChatStore = defineStore('chat-store', {
     async reloadRoute(uuid?: number) {
       this.recordState();
       mlog('toMyuid11','reloadRoute')
-      await sleep(1000)
+      await sleep(600)
       await router.push({ name: homeStore.myData.local=='draw'?'draw': 'Chat', params: { uuid } })
     },
 
