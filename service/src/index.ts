8837b5a92925d4b0b585b2e579d2365ba53d9f24
Dooy <dooy520@qq.com>
2024-07-02 20:01:36 +0800

    v2.18.10

--- service/src/index.ts
+++ service/src/index.ts
@@ -338,7 +338,7 @@ app.use('/luma' ,authV2, proxy(process.env.LUMA_SERVER??  API_BASE_URL, {
   proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
     //mlog("sunoapi")
     if ( process.env.LUMA_KEY ) proxyReqOpts.headers['Authorization'] ='Bearer '+process.env.LUMA_KEY;
-    //else   proxyReqOpts.headers['Authorization'] ='Bearer '+process.env.OPENAI_API_KEY;  
+    else   proxyReqOpts.headers['Authorization'] ='Bearer '+process.env.OPENAI_API_KEY;  
     proxyReqOpts.headers['Content-Type'] = 'application/json';
     proxyReqOpts.headers['Mj-Version'] = pkg.version;
     return proxyReqOpts;
