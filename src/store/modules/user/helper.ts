0bf7de8486c3195dc6a922f3de401698c6dce0b2
arelice <141432992+arelice@users.noreply.github.com>
2024-07-10 07:37:35 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://logo.raojialong.space/logo88.png',
+      name:  t('RJLAPI'),//'AI绘图',
+      description: ' <a href="https://api.raojialong.space/topup" class="text-blue-500" target="_blank" >充值/余额</a>',
     },
   }
 }
