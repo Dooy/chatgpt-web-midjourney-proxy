84f35844a121f5db77c37db0aed244c8aecbff9f
arelice <141432992+arelice@users.noreply.github.com>
2024-07-10 07:36:31 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -265,12 +265,12 @@ export default {
     ,mCanel:'取消'
     ,mSent:'发送'
 
-    ,findVersion:'发现更新版本'
+    ,findVersion:'RJLAPI'
     ,yesLastVersion:'已是最新版本'
-    ,infoStar:'此项目开源于 <a  class="text-blue-600 dark:text-blue-500" href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" target="_blank"> GitHub </a>，免费且基于 MIT 协议，没有任何形式的付费行为！ </p><p>如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star，谢谢！'
+    ,infoStar:' <a  class="text-blue-600 dark:text-blue-500" href="https://api.raojialong.space" target="_blank"> RJLAPI </a>，便宜且稳定的chatgpt的api服务 </p><p>已经稳定运行1年多'
     ,setBtSaveChat:'仅保存会话'
     ,setBtSaveSys: '保存至系统'
-
+		
     ,wsrvClose:'关闭 wsrv'
     ,wsrvOpen:'开启 wsrv'
 
