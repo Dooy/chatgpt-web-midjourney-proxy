a9323388cea354196bd49c1bfc88a34471cd6624
arelice <141432992+arelice@users.noreply.github.com>
2024-07-10 09:00:10 +0800

    Update aiSider.vue

--- src/views/mj/aiSider.vue
+++ src/views/mj/aiSider.vue
@@ -78,18 +78,21 @@ const chatId= computed(()=>chatStore.active??'1002' );
                     {{$t('mjtab.drawinfo')}}
                 </n-tooltip>
             </a>
-					<a :href="`#/comic`"
-          class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
-          <n-tooltip placement="right" trigger="hover">
-            <template #trigger>
-              <div class="flex h-full justify-center items-center   py-1 flex-col">
-                <SvgIcon icon="whh:spiderman" class="text-3xl flex-1"></SvgIcon>
-                <span class="text-[10px]">RJLAPI</span>
-              </div>
-            </template>
-            RJLAPI
-          </n-tooltip>
-        </a>
+					<a v-if="!isDisableMenu ( 'draws')"  @click="st.active='draw'; urouter.push(`/draw`)" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
+                <n-tooltip placement="right" trigger="hover">
+                  <template #trigger> 
+                    <div  class="flex h-full justify-center items-center   py-1 flex-col" :class="[goHome=='draw' ? 'active' : '']">
+                    <SvgIcon icon="ic:outline-palette" class="text-3xl flex-1"></SvgIcon>
+                     <span class="text-[10px]">{{$t('mjtab.draw')}}</span>
+                    </div> 
+                  </template>
+                    {{$t('mjtab.drawinfo')}}
+                </n-tooltip>
+            </a>
+           <a :href="`https://api.raojialong.space/topup`" class="flex items-center justify-center flex-col">
+      <SvgIcon icon="whh:spiderman" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">控制台</div>
+    </a>
 					<a :href="`#/bingAI`"
           class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
           <n-tooltip placement="right" trigger="hover">
