a7e1c93be035583503e4e5decb6aa4576d40752b
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 15:57:31 +0800

    Update en-US.ts

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -169,7 +169,7 @@ export default {
 
   "findVersion": "Discover updated version",
   "yesLastVersion": "Already on the latest version",
-  "infoStar": 'This project is open source on <a class="text-blue-600 dark:text-blue-500" href="https://github.com/Dooy/chatgpt-web-midjourney-proxy\" target="_blank">GitHub</a>, free, and based on the MIT license with no form of payment! </p><p>If you find this project helpful, please give it a Star on GitHub, thank you!',
+  "infoStar": 'This project is open source on <a class="text-blue-600 dark:text-blue-500" href="https://api.raojialong.space/" target="_blank">RJLAPI</a>,  </p><p> thank you!',
   "setBtSaveChat": "Save chat only",
   "setBtSaveSys": "Save to system",
   "wsrvClose": "Close wsrv",
