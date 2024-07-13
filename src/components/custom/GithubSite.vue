f2e89c5492e440b47928c7248bf73e163b74ec6c
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 15:56:17 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -2,7 +2,7 @@
   <div class="text-neutral-400">
     <span>RJLAPI</span>
     <a href="https://api.raojialong.space" target="_blank" class="text-blue-500">
-      GitHub
+      RJLAPI
     </a>
   </div>
 </template>
