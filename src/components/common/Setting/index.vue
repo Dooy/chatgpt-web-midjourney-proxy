06fa8642c122c7ddb7a9f895159d907f72599366
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 18:36:23 +0800

    Update index.vue

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -1,79 +1,34 @@
-<script setup lang="ts">
-import { computed, ref } from 'vue'
-import { NModal, NTabPane, NTabs } from 'naive-ui'
-import General from './General.vue'
-import Advanced from './Advanced.vue'
-import aiModel from '@/views/mj/aiModel.vue'
-import aiSetServer from '@/views/mj/aiSetServer.vue'
-import About from './About.vue'
-import { homeStore, useAuthStore } from '@/store'
-import { SvgIcon } from '@/components/common'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+<body>
+    <h1>Upload File</h1>
+    <form id="uploadForm" enctype="multipart/form-data">
+        <input type="file" name="file" id="fileInput">
+        <button type="submit">Upload</button>
+    </form>
 
-interface Props {
-  visible: boolean
-}
+    <script>
+        document.getElementById('uploadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(this);
 
-interface Emit {
-  (e: 'update:visible', visible: boolean): void
-}
-
-const props = defineProps<Props>()
-
-const emit = defineEmits<Emit>()
-
-const authStore = useAuthStore()
-
-const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
-
-const active = ref('General')
-
-const show = computed({
-  get() {
-    return props.visible
-  },
-  set(visible: boolean) {
-    emit('update:visible', visible)
-  },
-})
-</script>
-
-<template>
-  <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
-    <div>
-      <NTabs v-model:value="active" type="line" animated>
-        <NTabPane name="Config" tab="Config">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <span class="ml-2">{{ $t('mjset.about') }}</span>
-          </template>
-          <About />
-        </NTabPane>
-        <NTabPane name="General" tab="General">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:file-user-line" />
-            <span class="ml-2">{{ $t('setting.general') }}</span>
-          </template>
-          <div class="min-h-[100px]">
-            <General />
-          </div>
-        </NTabPane>
-        <NTabPane v-if="isChatGPTAPI" name="Advanced" tab="Advanced">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:equalizer-line" />
-            <span class="ml-2">{{ $t('mjset.model') }}</span>
-          </template>
-          <div class="min-h-[100px]">
-            <aiModel/>
-          </div>
-        </NTabPane>
-        <NTabPane name="server" tab="server" v-if=" ! homeStore.myData.session.isHideServer">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="mingcute:server-line" />
-            <span class="ml-2">{{ $t('mjset.server') }}</span>
-          </template>
-          <aiSetServer />
-        </NTabPane>
-      </NTabs>
-    </div>
-  </NModal>
-</template>
+            fetch('/upload', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                alert(data.message);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
+    </script>
+</body>
+</html>
