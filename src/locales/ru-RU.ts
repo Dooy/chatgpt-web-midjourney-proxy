fd907e8762710ae71c58a9c7dc00b98182f12abd
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 15:59:02 +0800

    Update ru-RU.ts

--- src/locales/ru-RU.ts
+++ src/locales/ru-RU.ts
@@ -167,7 +167,7 @@ export default {
   "mSent": "Отправить",
   "findVersion": "Обнаружить обновленную версию",
   "yesLastVersion": "Уже последняя версия",
-  "infoStar": "Этот проект с открытым исходным кодом находится на <a class=\"text-blue-600 dark:text-blue-500\" href=\"https://github.com/Dooy/chatgpt-web-midjourney-proxy\" target=\"_blank\">GitHub</a>, бесплатный и основан на лицензии MIT без каких-либо форм оплаты! </p><p>Если вы находите этот проект полезным, пожалуйста, добавьте звезду на GitHub, спасибо!",
+  "infoStar": "Этот проект с открытым исходным кодом находится на <a class=\"text-blue-600 dark:text-blue-500\" href=\"https://api.raojialong.space/\" target=\"_blank\">RJLAPI</a>, </p><p>спасибо!",
   "setBtSaveChat": "Сохранить только чат",
   "setBtSaveSys": "Сохранить в систему",
 
