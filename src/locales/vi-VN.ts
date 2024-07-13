77975604e5f9260f88cf74dd2ff0d7193d70ff21
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 15:59:37 +0800

    Update vi-VN.ts

--- src/locales/vi-VN.ts
+++ src/locales/vi-VN.ts
@@ -164,7 +164,7 @@ export default {
     "mSent": "Gửi",
     "findVersion": "Phát hiện phiên bản cập nhật",
     "yesLastVersion": "Đã là phiên bản mới nhất",
-    "infoStar": "Dự án này được mở nguồn tại <a class=\"text-blue-600 dark:text-blue-500\" href=\"https://github.com/Dooy/chatgpt-web-midjourney-proxy\" target=\"_blank\">GitHub</a>, miễn phí và dựa trên giấy phép MIT mà không có bất kỳ hình thức thanh toán nào! </p><p>Nếu bạn thấy dự án này hữu ích, hãy cho nó một sao trên GitHub, cảm ơn bạn!",
+    "infoStar": "Dự án này được mở nguồn tại <a class=\"text-blue-600 dark:text-blue-500\" href=\"https://api.raojialong.space/\" target=\"_blank\">RJLAPI</a>,  </p><p> bạn!",
     "setBtSaveChat": "Chỉ lưu trò chuyện",
     "setBtSaveSys": "Lưu vào hệ thống",
 
