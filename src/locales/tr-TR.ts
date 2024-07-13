27762261f7c5762831e0daa3dbf3a0fdf8ee70e1
arelice <141432992+arelice@users.noreply.github.com>
2024-07-13 15:59:17 +0800

    Update tr-TR.ts

--- src/locales/tr-TR.ts
+++ src/locales/tr-TR.ts
@@ -167,7 +167,7 @@ export default {
 
         "findVersion": "Güncellenmiş sürümü keşfet",
         "yesLastVersion": "Zaten en son sürümde",
-        "infoStar": 'Bu proje <a class="text-blue-600 dark:text-blue-500" href="https://github.com/Dooy/chatgpt-web-midjourney-proxy\" target="_blank">GitHub</a> üzerinde açık kaynaklı, ücretsiz ve MIT lisansına dayanmaktadır, herhangi bir ödeme şekli yoktur! </p><p>Bu projeyi yararlı bulursanız, lütfen GitHub üzerinde yıldız verin, teşekkür ederim!',
+        "infoStar": 'Bu proje <a class="text-blue-600 dark:text-blue-500" href="https://api.raojialong.space/" target="_blank">RJLAPI</a> </p><p> ederim!',
         "setBtSaveChat": "Sadece sohbeti kaydet",
         "setBtSaveSys": "Sisteme kaydet",
         "wsrvClose": "wsrv'yi kapat",
