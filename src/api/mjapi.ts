d2e87fa9f163b735db66a136c2f76313f4a696b0
Dooy <dooy520@qq.com>
2024-08-14 15:03:01 +0800

    v2.20.2

--- src/api/mjapi.ts
+++ src/api/mjapi.ts
@@ -375,6 +375,9 @@ export const canVisionModel= (model:string)=>{
 export const isCanBase64Model=(model:string)=>{
     //gpt-4o
     //customVisionModel
+    if(model.indexOf('gpt-4o')>-1 || ( model.indexOf('gemini')>-1 && model.indexOf('1.5')>-1 ) ){
+        return true
+    }
     let visionArr=['gemini-pro-vision','gpt-4o-2024-08-06','gpt-4o','gpt-4o-2024-05-13','gpt-4o-mini','gpt-4o-mini-2024-07-18','gemini-pro-1.5','gpt-4-turbo','gpt-4-turbo-2024-04-09','gpt-4-vision-preview','luma-video','claude-3-5-sonnet-20240620' ,'claude-3-sonnet-20240229','claude-3-opus-20240229', defaultVisionModel() ]
     if( homeStore.myData.session.customVisionModel ){ 
         homeStore.myData.session.customVisionModel.split(/[ ,]+/ig).map( (v:string)=>{
