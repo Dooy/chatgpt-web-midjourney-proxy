17437521c0ff7a47ba9447b370c207f25f02810a
Dooy <dooy520@qq.com>
2025-07-18 10:03:48 +0800

    2.25.1

--- src/views/suno/mcInput.vue
+++ src/views/suno/mcInput.vue
@@ -14,12 +14,12 @@ const exSuno= ref<SunoMedia>()
 const des= ref( {
   "gpt_description_prompt": "",
   "make_instrumental": false,
-  "mv": "chirp-bluejay-t1",
+  "mv": "chirp-bluejay",
   "prompt": ""
 });
 const cs= ref({
   "prompt": "",
-  "mv": "chirp-bluejay-t1",
+  "mv": "chirp-bluejay",
   "title": "",
   "tags": "",
   "continue_at": 120,
@@ -33,7 +33,7 @@ const mvOption= [
 ,{label:'verion: v3',value: 'chirp-v3-0'}
 ,{label:'verion: v4',value: 'chirp-v4'}
 ,{label:'verion: v4.5',value: 'chirp-auk'}
-,{label:'verion: v4.5+',value: 'chirp-bluejay-t1'}
+,{label:'verion: v4.5+',value: 'chirp-bluejay'}
  ]
 
 const canPost = computed(() => {
